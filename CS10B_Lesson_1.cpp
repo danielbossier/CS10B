dfb112a25d1ce51e62e313d1150610b2f88ffb63
danielbossier <85263571+danielbossier@users.noreply.github.com>
2025-06-15 21:49:59 -0700

    Add files via upload

---
+++ CS10B_Lesson_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+    using namespace std;
+
+    int main() {
+        int hoursWorked;
+        int paycheckAmount;
+        int payrate;
+
+        cout << "enter hours worked: ";
+        cin >> hoursWorked;
+        cout << "Enter rate of pay: ";
+        cin >> payrate;
+        paycheckAmount = hoursWorked * payrate;
+        cout << "The amount of the paycheck is $"
+             << paycheckAmount << " dollars." << endl;
+    }
